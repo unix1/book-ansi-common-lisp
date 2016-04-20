3d97e9d4b267f8dfa2feb7ddc31226d40046353e
Unix1 <unix1@live.com>
2016-04-19 22:15:45 -0700

    solution to ch 6 ex 2, and add remaining exercises

--- ch-6/exercises.lisp
+++ ch-6/exercises.lisp
@@ -20,9 +20,49 @@
        (not (char= c #\  ))))
 
 ; 2. Define a version of bin-search (page 60) that takes :key, :test, :start
-; and :end argumentswith the usual meanings and defaults.
+; and :end arguments with the usual meanings and defaults.
+(defun bin-search (obj vec &key (key #'identity) (test #'eql) (start 0) (end nil))
+  (let ((len (if (null end)
+                 (- (length vec) 1)
+                 end)))
+    (and (not (zerop len))
+         (finder obj vec key test start len))))
+
+(defun finder (obj vec key test start end)
+  (let ((range (- end start)))
+    (if (zerop range)
+        (if (funcall test obj (funcall key (aref vec start)))
+            obj
+            nil)
+        (let ((mid (+ start (round (/ range 2)))))
+          (let ((obj2 (funcall key (aref vec mid))))
+            (if (< obj obj2)
+                (finder obj vec key test start (- mid 1))
+                (if (> obj obj2)
+                    (finder obj vec key test (+ mid 1) end)
+                    obj)))))))
 
 ; 3. Define a function that takes any number of arguments and returns the
 ; number of arguments passed to it.
 (defun num-args (&rest args)
   (length args))
+
+; 4. Modify most (page 105) to return, as two values, the two highest-scoring
+; elements of a list.
+
+; 5. Define remove-if (no keywords) in terms of filter (page 105)
+
+; 6. Define a function that takes one argument, a number, and returns the
+; greatest argument passed to it so far.
+
+; 7. Define a function that takes one argument, a number, and returns true if
+; it is greater than the argument that was passed to the function the last time
+; it was called. The function should return nil the first time it is called.
+
+; 8. Supposed expensive is a function of one argument, an integer between 0 and
+; 100 inclusive, that returns the result of a time-consuming computation.
+; Define a function frugal that returns the same answer, but only calls
+; expensive when given an argument it has not seen before.
+
+; 9. Define a function like apply, but where any number printed out before it
+; returns will be printed, by default, in octal (base 8).
